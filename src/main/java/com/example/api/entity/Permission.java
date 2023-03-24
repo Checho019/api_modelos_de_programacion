6d4f009adfd013a3db5af53af5a3e41fd17902bf
Sergio-Linux <sergioduarte20013@gmail.com>
2023-03-23 21:03:05 -0500

    Lista de cambios: - Creacion de relacion muchos a muchos entre permission y role - Creacion de servicio de guardado de permission y su respectivo endpoint

--- src/main/java/com/example/api/entity/Permission.java
+++ src/main/java/com/example/api/entity/Permission.java
@@ -1,15 +1,20 @@
 package com.example.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "permission"
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -21,4 +26,9 @@ public class Permission {
     @NotEmpty(message = "permission cannot be empty")
     @Column(name = "permission_name")
     private String permission;
+
+    @ManyToMany(mappedBy = "permissions")
+    @JsonIgnore
+    private Set<Role> roles;
+
 }
